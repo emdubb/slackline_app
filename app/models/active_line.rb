60a861c99f8601e3435d5079021ed2d3711ed1fc
Melissa Wilcox <melissa@emdubb.co>
2015-11-03 15:41:16 -0800

    add methods to line and activeline models

--- app/models/active_line.rb
+++ app/models/active_line.rb
@@ -1,7 +1,23 @@
 class ActiveLine < ActiveRecord::Base
   belongs_to :line
-  after_validation :geocode
+
+  # validations
+  # ...
+
+  # set up methods (outside gems)
   geocoded_by :place
+
+  # hooks!
+  before_create :set_started_at
+  after_validation :geocode
+
+  def is_up?
+    finished_at.nil?
+  end
+
+  def set_started_at
+    self.started_at = Time.now
+  end
 
   def place
     #the address or place
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,20 @@
 class Line < ActiveRecord::Base
   belongs_to :user
   has_many :active_lines
+
+  def is_active_line?
+    active_lines.any? {|al| al.is_up? }
+  end
+
+  def activate!
+    active_lines.create unless is_active_line?
+  end
+
+  def deactivate!
+    current_active_line.finished_at = Time.now if is_active_line?
+  end
+
+  def current_active_line
+    active_lines.find {|al| al.is_up? }
+  end
 end
