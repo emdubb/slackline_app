e1894a2fc0d221cdd643446bab1179968cab6ebe
Melissa Wilcox <melissa@emdubb.co>
2015-11-03 16:06:22 -0800

    update view to allow user to post a line

--- app/controllers/active_lines_controller.rb
+++ app/controllers/active_lines_controller.rb
@@ -1,5 +1,11 @@
 class ActiveLinesController < ApplicationController
   def index
+    @active_lines = ActiveLine.all
+  end
 
+  def create
+    line = Line.find(params[:line_id])
+    line.activate!
+    redirect_to user_path(current_user)
   end
 end
