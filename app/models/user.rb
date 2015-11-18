3518998aa3698f14b21a16a8cb25aa3284348b1b
Melissa Wilcox <melissa@emdubb.co>
2015-11-17 23:33:02 -0800

    add field validation to user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :lines, dependent: :destroy
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :skill, presence: true
+  validates :location, presence: true
 end
