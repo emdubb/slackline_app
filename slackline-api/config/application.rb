80f8da634bbbf36486c20c778af1fd4c10f756aa
Melissa Wilcox <melissa@emdubb.co>
2015-12-29 17:00:19 -0500

    allow CORS

--- slackline-api/config/application.rb
+++ slackline-api/config/application.rb
@@ -30,6 +30,15 @@ module SlacklineApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*',
+            :headers => :any,
+            :methods => [:get, :post, :patch, :put, :delete, :options]
+      end
+    end
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
