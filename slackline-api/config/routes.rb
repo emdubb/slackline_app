513fb0d256dbcbf3ecffe8af2e4904867e921fc0
Melissa Wilcox <melissa@emdubb.co>
2016-01-30 14:32:27 -0800

    add missing comma for nested route

--- slackline-api/config/routes.rb
+++ slackline-api/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'api#index'
 
   namespace :api do
-    resources :users, only: [:index, :new, :create, :show, :update], shallow: true do
-      resources :lines, shallow: true do
-        resources :active_lines
+    resources :users, only: [:index, :create, :show, :update, :destroy], shallow: true do
+      resources :lines, only: [:index, :create, :update, :destroy], shallow: true do
+        resources :active_lines, only: [:index, :create, :update]
       end
     end
     # resources :sessions, only: [:new, :create, :destroy]
