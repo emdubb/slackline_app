ecdad370e48fb9f773cdc4e0fee5adef8bff1811
Melissa Wilcox <melissa@emdubb.co>
2016-01-31 15:21:04 -0800

    add http request to get all active posts add method to show more line info

--- www/components/lines/lines.controller.js
+++ www/components/lines/lines.controller.js
@@ -10,6 +10,23 @@
   function LinesController($state, $log, $http, localStorageService){
     var vm = this;
     vm.test = "LinesController linked!"
+    vm.showLine = showLine
+
+    $http({
+      method: "GET",
+      url: "http://localhost:3000/api/posts?current=true",
+      contentType: "application/json",
+    }).then(function successCallback(response){
+      $log.log("active posts: ", response.data)
+      vm.posts = response.data
+    }, function errorCallback(error){
+      $log.log(error)
+    })
+
+    function showLine() {
+      $log.log("show line")
+    }
+
   }
 
 })();
